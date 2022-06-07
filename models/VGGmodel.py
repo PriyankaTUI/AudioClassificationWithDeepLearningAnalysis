45e8f64e47c26af73d5b8ec42ba00df0663f7a9d
PriyankaTUI <priyanka_yadav01@yahoo.com>
2022-06-07 13:48:16 +0200

    add new nodes in model

--- models/VGGmodel.py
+++ models/VGGmodel.py
@@ -63,4 +63,37 @@ class VGGNet(nn.Module):
     x = self.conv4(x)
     x = self.flatten(x)
     logits = self.linear(x)
-    return x
+    return logits
+
+  def addNovelClassesToModel(self, noNovelClasses):
+      #add new novel classes to pre-trained model
+      #add new class parameters , here we have added 3 new classes
+      input_features = self.linear.in_features
+      output_features = self.linear.out_features
+      
+      print("Old Model:")
+      summary(model,input_size=(1,32,32))
+
+      #save fc3 layers weights for initialization step
+      old_weights = copy.deepcopy(self.linear.weight.data)  #torch.Size([10, 256])
+
+      new_output_features = output_features + noNovelClasses
+      print(f"new output features: {new_output_features}")
+
+      self.linear = nn.Linear(in_features=input_features, out_features= new_output_features)
+
+      #Initialize new weights with kaiming normal distribution 
+      # in paper xaviour is suggested for relu kaiming is recommended
+      nn.init.kaiming_normal_(self.linear.weight, nonlinearity='relu')
+      #copy old weights for old paramerter
+      self.linear.weight.data[:output_features] = old_weights
+
+      input_features = self.linear.in_features
+      output_features = self.linear.out_features
+      print(f"New model input features: {input_features}")
+      print(f"New model output features: {output_features}")
+      # print(f"New model; {self.model}")
+      print("New Model:")
+      summary(self,input_size=(1,32,32))
+
+      return
